8d691c884c882579d6dfb7dc4cfc16ebfdc47191
mindziukasss <mindziukasss@gmail.com>
2017-02-27 13:09:39 +0200

    Created db

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
+<?php include 'config/config.php'; ?>
+<?php include 'libraries/db.php'; ?>
 <?php include 'includes/header.php'; ?>
+<?php
+  
+  $db = new Db();
+
+?>
+
 <div class="blog-post">
   <h2 class="blog-post-title">International PHP Conference 2017</h2>
     <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
---
+++ libraries/db.php
@@ -0,0 +1,70 @@
+<?php
+
+class db{
+  public $host = DB_HOST;
+  public $username = DB_USER;
+  public $password = DB_PASS;
+  public $db_name = DB_NAME;
+
+
+  public $link;
+  public $error;
+
+  public function __construct(){
+  
+    $this->connect();
+
+  }
+
+  private function connect (){
+    $this->link = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+
+    if(!$this->link) {
+      $this->error = "Connection Failed: ".$this->link->connect_error;
+      return false;
+    }
+  }
+
+  public function select($query){
+    $result = $this->link->query($query) or die($this->link->error.__LINE__);
+    if($result->num_rows > 0){
+      return $result;
+    }else {
+      return false;
+    }
+  }
+
+  public function insert($query){
+    $insert_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+    if ($insert_row){
+      header("Location: index.php?msg=".urlencode('Record Added'));
+      exit();
+    }else {
+      die('Error : ('. $this->link->errno.') '. $this->link->error);
+    }
+  }
+
+  public function update($query){
+    $update_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+    if ($update_row){
+      header("Location: index.php?msg=".urlencode('Record Update'));
+      exit();
+    }else {
+      die('Error : ('. $this->link->errno.') '. $this->link->error);
+    }
+  }
+
+  public function delete($query){
+    $delete_row = $this->link->query($query) or die($this->link->error.__LINE__);
+
+    if ($delete_row){
+      header("Location: index.php?msg=".urlencode('Record Delete'));
+      exit();
+    }else {
+      die('Error : ('. $this->link->errno.') '. $this->link->error);
+    }
+  }
+
+}
